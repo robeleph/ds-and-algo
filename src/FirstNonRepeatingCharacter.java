e9a14732c3e72fb34b9e2a20d412654911c3c55f
robel_ephraim <ephraimrobel15@gmail.com>
2022-02-01 10:52:16 +0300

    first non-repeating character

---
+++ src/FirstNonRepeatingCharacter.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class FirstNonRepeatingCharacter {
+    public int firstNonRepeatingCharacter(String string) {
+        // Write your code here.
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+
+        for(int idx = 0; idx < string.length(); idx++){
+            char character = string.charAt(idx);
+            map.put(character, map.getOrDefault(character, 0) + 1);
+        }
+        for(int idx = 0; idx < string.length(); idx++){
+            char character = string.charAt(idx);
+            if(map.get(character) == 1){
+                return idx;
+            }
+        }
+        return -1;
+    }
+}
