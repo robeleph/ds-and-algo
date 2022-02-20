94c5094f3dcef4c08360bf69a7e4cc4796467b3e
Robel Ephraim <37698459+robeleph@users.noreply.github.com>
2022-02-20 14:18:58 +0300

    nth fibonacci

---
+++ NthFibonacci.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class NthFibonacci {
+  public static int getNthFib(int n) {
+		if(n == 2){
+			return 1;
+		}else if (n == 1){
+			return 0;
+		}else{
+			return getNthFib(n - 1) + getNthFib(n-2);
+		}
+  }
+}
