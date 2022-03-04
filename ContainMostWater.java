4d879bdb6db1182efef6cbd7a0b39c5de1fa76c0
Robel Ephraim <37698459+robeleph@users.noreply.github.com>
2022-03-04 22:21:26 +0300

    contain most water

---
+++ ContainMostWater.java
@@ -0,0 +1,23 @@
+class ContainMostWater {
+    public int maxArea(int[] height) {
+        int h = height[0];
+        int w = height.length - 1;
+        int area = 0;
+        
+        int j = height.length - 1;
+        int i = 0; 
+        System.out.println(w + " - " + j);
+        while(i < w && j > 0){
+            if (height[i] < height[j]) {
+                int cval = height[i] * Math.abs(j - i);
+                area = area < cval ? cval : area;
+                i++;
+            } else{
+                int cval = height[j] * Math.abs(j - i);
+                area = area < cval ? cval : area;
+                j--;
+            }
+        }
+        return area;
+    }
+}
