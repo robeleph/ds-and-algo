fbef3aa685b4a6a15fb851e9f63b340b2b865f01
Robel Ephraim <37698459+robeleph@users.noreply.github.com>
2022-03-28 00:54:02 -0700

    search in rotated array

---
+++ SearchInRotatedArray.java
@@ -0,0 +1,26 @@
+class SearchInRotatedArray {
+    public int search(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target)
+                return mid;
+            // nums[left to mid] is sorted
+            if (nums[l] <= nums[mid]) {
+                if (target >= nums[l] && target < nums[mid])
+                    r = mid - 1;
+                else
+                    l = mid + 1;
+            } 
+            // nums[mid to right] is sorted
+            else {
+                if (target > nums[mid] && target <= nums[r])
+                    l = mid + 1;
+                else
+                    r = mid - 1;   
+            }
+        }
+        return -1;
+    } 
+}
