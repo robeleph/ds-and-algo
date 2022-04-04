d87d773c7c32c16f835f7c0aca7d957aa4fcaaf6
Robel Ephraim <37698459+robeleph@users.noreply.github.com>
2022-04-04 02:01:00 -0700

    search insert position

---
+++ SearchInsertPositionII.java
@@ -0,0 +1,18 @@
+class SearchInsertPositionII {
+    public int searchInsert(int[] nums, int target) {
+        if(nums.length == 0 || nums == null) return 0;
+        int start = 0;
+        int end = nums.length - 1;
+        while(start <= end){
+            int mid = (start + end) / 2;
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] > target){
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+}
