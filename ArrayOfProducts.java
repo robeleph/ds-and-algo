0e1c1bd1030ab8dd946510d515b923117a8d479b
robeleph <ephraimrobel15@gmail.com>
2024-01-30 10:55:13 -0700

    code templates

--- ArrayOfProducts.java
+++ ArrayOfProducts.java
@@ -4,18 +4,18 @@ class ArrayOfProducts {
   public int[] arrayOfProducts(int[] array) {
     int[] solution = new int[array.length];
 		
-		int leftProduct = 1;
-		int rightProduct = 1;
-		
-		for (int i = 0; i < array.length; i++) {
-			solution[i] = leftProduct;
-			leftProduct *= array[i];
-		}
-		
-		for (int j = array.length - 1; j >= 0; j--) {
-			solution[j] *= rightProduct;
-			rightProduct *= array[j];
-		}
+	int leftProduct = 1;
+	int rightProduct = 1;
+	
+	for (int i = 0; i < array.length; i++) {
+		solution[i] = leftProduct;
+		leftProduct *= array[i];
+	}
+	
+	for (int j = array.length - 1; j >= 0; j--) {
+		solution[j] *= rightProduct;
+		rightProduct *= array[j];
+	}
 		
     return solution;
   }
